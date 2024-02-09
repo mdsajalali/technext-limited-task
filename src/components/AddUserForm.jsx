aae6afdbe6fa934b8fba9ec78627ebf1378dd49a
mdsajalali <mdsajalali2020@gmail.com>
2024-02-09 23:10:37 +0600

    add user from section styles added

--- src/components/AddUserForm.jsx
+++ src/components/AddUserForm.jsx
@@ -10,6 +10,7 @@ const AddUserForm = () => {
     const lastName = form.lastName.value;
     const email = form.email.value;
     const address = form.address.value;
+    const city = form.city.value;
     const companyName = form.companyName.value;
     const avatarUrl = form.avatarUrl.value;
 
@@ -19,6 +20,7 @@ const AddUserForm = () => {
       lastName,
       email,
       address,
+      city,
       companyName,
       avatarUrl,
     };
@@ -30,55 +32,107 @@ const AddUserForm = () => {
   };
 
   return (
-    <div className="max-w-[1200px] mx-auto">
+    <div className="max-w-[1200px] mx-auto my-10">
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className="w-full">
           <input
             type="url"
             placeholder="Avatar URL"
             name="avatarUrl"
             required
+            className="w-full border p-2 outline-none focus:border-[#06D6A0]"
           />
         </div>
 
-        <div>
+        <div className="w-full flex items-center justify-center gap-10 my-5">
           <input
             type="text"
             placeholder="First Name"
             name="firstName"
             required
+            className="w-full border p-2 outline-none focus:border-[#06D6A0]"
           />
 
-          <input type="text" placeholder="Last Name" name="lastName" required />
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            required
+            className="w-full border p-2 outline-none focus:border-[#06D6A0]"
+          />
         </div>
 
-        <input type="email" placeholder="Email" name="email" required />
+        <div className="w-full flex items-center justify-center gap-10 mt-5">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            required
+            className="w-full border p-2 outline-none focus:border-[#06D6A0]"
+          />
+          <input
+            type="text"
+            placeholder="Address"
+            name="address"
+            required
+            className="w-full border p-2 outline-none focus:border-[#06D6A0]"
+          />
+        </div>
 
-        <div>
-          <input type="text" placeholder="Address" name="address" required />
+        <div className="w-full flex items-center justify-center gap-10 mt-5">
+          <input
+            type="text"
+            placeholder="City"
+            name="city"
+            required
+            className="w-full border p-2 outline-none focus:border-[#06D6A0]"
+          />
 
           <input
             type="text"
             placeholder="Company Name"
             name="companyName"
             required
+            className="w-full border p-2 outline-none focus:border-[#06D6A0] "
           />
         </div>
 
-        <button type="submit">Add user</button>
+        <button
+          className="w-full bg-[#06D6A0] my-5 text-white py-2 text-[20px] rounded-md"
+          type="submit"
+        >
+          Add user
+        </button>
       </form>
       {/* Show submitted data */}
-      {submittedData.map((data) => (
-        <div key={data.id}>
-          <h2>Submitted Data:</h2>
-          <img src={data.avatarUrl} alt="" />
-          <p>First Name: {data.firstName}</p>
-          <p>Last Name: {data.lastName}</p>
-          <p>Email: {data.email}</p>
-          <p>Address: {data.address}</p>
-          <p>Company Name: {data.companyName}</p>
-        </div>
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 mb-14 mt-5 lg:grid-cols-3 gap-10">
+        {submittedData.map((data) => (
+          <div
+            key={data.id}
+            className="shadow-md rounded-md transform transition-transform hover:scale-105"
+          >
+            <div>
+              <img
+                className="w-[250px] object-cover mx-auto my-2"
+                src={data.avatarUrl}
+                alt=""
+              />
+            </div>
+            <div className="px-5 my-5 text-center">
+              <div className="flex items-center justify-center gap-1 text-[18px] font-semibold">
+                <h1>Name:</h1>
+                <h1>{data.firstName}</h1>
+                <h1>{data.lastName}</h1>
+              </div>
+
+              <p className="text-[16px] font-medium">Email: {data.email}</p>
+              <p>Address: {data.address}</p>
+              <p>City: {data.city}</p>
+              <p>Company: {data.companyName}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
